1582bfe40d860d074bdfeda3d8ca80707ebae1d1
MARISOL PUNTUSIL ORMACHEA <marisol.pormachea@senacsp.edu.br>
2024-10-08 17:27:24 -0300

    first commit

---
+++ EXERCICIO-1/script.js
@@ -0,0 +1,39 @@
+// saudaco("Maria"); 
+//  (Olá,{Maria});
+//  console.log {olá(Maria);
+//  }
+
+// funtion saudacao(nome){
+//     return `olá,${nome}`
+// }
+// console.log(saudac("Joaquim"))
+
+
+// funtion multiplicacao(a,b){
+
+// 4Crie uma função chamada parOuImpar que recebe um número e retorna "Par" ou "Ímpar" dependendo da verificação.
+// funtion emPar(numero) {
+//     if (numero % 2==)
+//         return true
+//     } else  {
+//     return false
+
+// }
+4
+// // Exemplo de uso
+
+// funtion OuImpar numero[[
+//     if(numero % 2==)
+// ]]
+// funtio media (num1,num2,num3,){
+
+// }
+// 5
+// funtion ahPalindromo(str)
+// let palavra = str.replace(/\s+/g,``).tolowerCase();
+
+
+
+// funtion numeroAleatorio(min,max)[
+//     return Math.floor(Math.random)()* (max - min +)
+// ]
